442e4149e3b3d6774ca6999aeb13fa78dbf15070
andreatito0 <andrea.tito@studenti.unipd.it>
2025-05-06 15:24:35 +0200

    #4 implementato test per indici fuori limite

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -2,3 +2,16 @@
 // Thomas       Morettin    2111001
 // Andrea       Tito        2111033
 ////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class IntegerToRomanTest {
+    @Test
+    public void test_IllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> IntegerToRoman.convert(0));
+        assertThrows(IllegalArgumentException.class, () -> IntegerToRoman.convert(1001));
+    }
+}
--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -2,3 +2,17 @@
 // Thomas       Morettin    2111001
 // Andrea       Tito        2111033
 ////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class RomanPrinterTest {
+    @Test
+    public void test_IllegalArgumentExceptio() {
+        assertThrows(IllegalArgumentException.class, () -> RomanPrinter.print(0));
+        assertThrows(IllegalArgumentException.class, () -> RomanPrinter.print(1001));
+    }
+}
